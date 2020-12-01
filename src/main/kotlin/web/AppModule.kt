b174069303f444e4056f36f71b9e1a4c7981c458
16dprice <donald.price077@topper.wku.edu>
2020-11-30 20:58:27 -0600

    Add basic web routing (#1)
    * add ktor dependency
    
    * add main class that will launch the web server
    
    * remove boilerplate routes

---
+++ src/main/kotlin/web/AppModule.kt
@@ -0,0 +1,16 @@
+package web
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.DefaultHeaders
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.route
+import io.ktor.routing.get
+
+fun Application.appModule() {
+    install(DefaultHeaders)
+    install(Routing) {
+    }
+}
