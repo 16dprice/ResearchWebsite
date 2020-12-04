3beb24dd52f5ca76ffb8a3f36636c70ccee66052
DJ <donald.price077@topper.wku.edu>
2020-12-03 18:25:11 -0600

    remove unused imports

--- src/main/kotlin/web/AppModule.kt
+++ src/main/kotlin/web/AppModule.kt
@@ -1,13 +1,9 @@
 package web
 
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.DefaultHeaders
-import io.ktor.response.respondText
 import io.ktor.routing.Routing
-import io.ktor.routing.route
-import io.ktor.routing.get
 
 fun Application.appModule() {
     install(DefaultHeaders)
