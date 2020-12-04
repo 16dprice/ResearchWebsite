a3344489bba120ca43c7edf12d5b9175a18a1582
DJ <donald.price077@topper.wku.edu>
2020-12-03 19:24:26 -0600

    basic landing page and ktor routing

--- src/main/kotlin/web/AppModule.kt
+++ src/main/kotlin/web/AppModule.kt
@@ -1,12 +1,45 @@
 package web
 
+import dev.jbs.ktor.thymeleaf.Thymeleaf
+import dev.jbs.ktor.thymeleaf.ThymeleafContent
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.http.content.static
+import io.ktor.response.respond
 import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
 fun Application.appModule() {
     install(DefaultHeaders)
+    install(CallLogging)
+    install(Thymeleaf) {
+        setTemplateResolver(ClassLoaderTemplateResolver().apply {
+            prefix = "templates/"
+            suffix = ".html"
+            characterEncoding = "utf-8"
+        })
+    }
     install(Routing) {
+        routing {
+            static("static") {
+
+            }
+        }
+        route("/") {
+            get {
+                call.respond(
+                    ThymeleafContent(
+                        template = "landing",
+                        model = mapOf()
+                    )
+                )
+            }
+        }
     }
 }
